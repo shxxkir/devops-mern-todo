bbee67c05f3e12efabb12c458884ba353edf37eb
shxxkir <128359891+shxxkir@users.noreply.github.com>
2025-09-18 01:06:34 +0530

    Initial Commit

---
+++ backend/tests/tasks.test.js
@@ -0,0 +1,19 @@
+const request = require('supertest'); 
+const { server,app } = require('../index.js');
+const mongoose = require('mongoose');
+
+describe('GET api/tasks', () => {
+  it('should return 200 ok', async () => {
+    const res = await request(app).get('/api/tasks');
+    expect(res.statusCode).toBe(200);
+  });
+  it('should return an array', async () => {
+    const res = await request(app).get('/api/tasks');
+    expect(Array.isArray(res.body)).toBe(true);
+  });
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+  server.close();
+});
